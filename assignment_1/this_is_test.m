bd38470e02db39945c37e6681009b714900851c5
NSavov <nedko066@gmail.com>
2017-04-05 15:13:57 +0200

    educational conflict

--- assignment_1/this_is_test.m
+++ assignment_1/this_is_test.m
@@ -1,4 +1,4 @@
-function [ output_args ] = this_is_test( input_args )
+function [ output_args ] = this_is-test( input_args )
 %THIS_IS_TEST Summary of this function goes here
 %   Detailed explanation goes here
 
