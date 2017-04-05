f378156ea9cb7d0c5b5bb90d7921899901577fec
Joop Pascha <j.pascha@gmail.com>
2017-04-05 19:13:59 +0200

    blah

--- assignment_1/get_rms_error.m
+++ assignment_1/get_rms_error.m
@@ -1,7 +1,7 @@
 function [ error ] = get_rms_error(Source_pc, R, t)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-    Transformed_source_pc = Source_pc*R + t;
-    error = sum(sqrt(sum((Transformed_source_pc - Source_pc).^2, 2)));
+    Transformed_source_pc = Source_pc'*R + t';
+    error = sum(sqrt(sum((Transformed_source_pc - Source_pc').^2, 2)));
 end
 
