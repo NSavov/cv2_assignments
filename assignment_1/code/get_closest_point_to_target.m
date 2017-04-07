ffb2e66db8d200d347ffdd01b51e0c43ddf0a9ba
Joop Pascha <j.pascha@gmail.com>
2017-04-07 15:12:49 +0200

    are we finally going to be on a working version?

--- assignment_1/code/get_closest_point_to_target.m
+++ assignment_1/code/get_closest_point_to_target.m
@@ -1,8 +1,7 @@
-function [ Closest_points_pc ] = get_closest_point_to_target(Source_pc, Target_pc, sampling_technique, sample_size, R, t)
+function [ Closest_points_pc ] = get_closest_point_to_target(Source_pc, Target_pc, R, t)
 %FIND_CLOSEST_POINT_TO_TARGET Summary of this function goes here
 %   Detailed explanation goes here
     Source_pc = R*Source_pc + t;
-    sample(Source_pc, sampling_technique, sample_size);
     Closest_points_pc_ind = knnsearch(Target_pc', Source_pc');
     Closest_points_pc = Target_pc(:,Closest_points_pc_ind);
 end
