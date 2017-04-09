ff98676556cae2d7fb0fbb1224eb485d97e41cfc
NSavov <nedko066@gmail.com>
2017-04-09 15:35:54 +0200

    implemented spatial uniform sampling

--- assignment_1/code/sample.m
+++ assignment_1/code/sample.m
@@ -7,7 +7,29 @@ function [ Sampled_data ] = sample(Data, sampling_technique, sample_size )
             Sampled_data = Data;
         case 'uniform' 
             step = size(Data, 2)/sample_size;
-            Sampled_data = Data(:, 1:step:end);
+            pc_size = inf;
+            block_size = 0.01;
+            block_size_step = 0.01;
+            pc = pointCloud(Data');
+            Sampled_data_new = [];
+            
+            while pc_size > sample_size
+                pc_sampled = pcdownsample(pc,'gridAverage',block_size);
+                Sampled_data = Sampled_data_new;
+                Sampled_data_new = pc_sampled.Location';
+                pc_size = size(Sampled_data_new, 2);
+                block_size = block_size + block_size_step;
+            end
+            
+            Sampled_data = sample(Sampled_data, 'random', sample_size);
+            
+            %use this for visualization:
+            %pcshow(pointCloud(Sampled_data'));
+            
+            %old way:
+%             Sampled_data = Data(:, 1:step:end);
+            
+            
         case 'random'
             Sampled_data = datasample(Data,sample_size, 2, 'Replace', false);
 %         case 'informed_sampling'
