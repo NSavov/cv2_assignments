d5c974720390860d1c564265f2bb771fb3531910
Joop Pascha <j.pascha@gmail.com>
2017-04-11 16:36:43 +0200

    more

--- assignment_1/main.m
+++ assignment_1/main.m
@@ -8,19 +8,14 @@ delete plots/*.fig
 
 sampling_techniques = {'allpoints', 'uniform', 'random', 'informed'};
 sampling_technique = 'informed';
-<<<<<<< Updated upstream
-sample_size = 30000;
+sample_size = 3000;
 threshold = 0.0001;
-=======
-sample_size = 1000;
-threshold = 0.000;
->>>>>>> Stashed changes
+
 % for technique = techniques
     temp = load('source.mat');
     Source_pc = temp.source;
     temp = load('target.mat');
     Target_pc = temp.target;
-<<<<<<< Updated upstream
     
     Source_pc(end+1,:) = 1;
     Target_pc(end+1,:) = 1;
@@ -28,7 +23,4 @@ threshold = 0.000;
     start_t = tic;
     icp_algorithm(Source_pc, Target_pc, threshold, sampling_technique, sample_size)
     toc(start_t)
-=======
-    icp_algorithm(Source_pc, Target_pc, threshold, sampling_technique, sample_size);
->>>>>>> Stashed changes
 % end
