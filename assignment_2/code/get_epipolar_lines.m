5b5f59830c34ed028d7700302f7cc95ce8cba180
NSavov <nedko066@gmail.com>
2017-05-04 10:11:46 +0200

    forgot to add epipolar lines plotting code

---
+++ assignment_2/code/get_epipolar_lines.m
@@ -0,0 +1,10 @@
+function [  target_points_y ] = get_epipolar_lines( F, source_points, target_points_x)
+    source_points(end+1, :) = 1;
+    line = F*source_points;
+    a = line(1, :);
+    b = line(2, :);
+    c = line(3, :);
+    
+    target_points_y = (-a'*target_points_x-c')./b';
+
+end
