653d6f30afebc510446139f2b51cc6df61b3b91b
Joop Pascha <j.pascha@gmail.com>
2017-05-09 12:19:33 +0200

    redundant construct_point_view_matri

--- assignment_2/main.m
+++ assignment_2/main.m
@@ -5,14 +5,14 @@ addpath code/plotting_and_printing
 addpath code/structure_from_motion
 run ./vlfeat/toolbox/vl_setup.m
 
-trials = 100; 
+trials = 300; 
 outlier_threshold = 1;
 sample_count = 20;
 
 img_names = get_image_names_from_directory('data/', 'png');
 offset = 0;
 im1 = imread(img_names{1+offset});
-match_list = {};
+descriptor_list = {};
 for img_n = {img_names{2+offset:end}}
     clf
     im2 = imread(img_n{1});
@@ -21,12 +21,10 @@ for img_n = {img_names{2+offset:end}}
     [F, inlier_indices] = get_fundamental_matrix(f1, f2, matches, trials, sample_count, outlier_threshold);
 
     % setup for construct_pointview_matrix
-    match_list{end+1} = {f1, d1};
-    size(match_list{1}{2})
-    if isequal(img_names{end},img_n{1})
-
-        match_list{end+1} = {f2, d2};
-    end
+    descriptor_list{end+1} = {f1, d1, f2, d2, matches, inlier_indices};
+%     if isequal(img_names{end},img_n{1})
+%         descriptor_list{end+1} = {f2, d2};
+%     end
     
     im1 = im2;
     selected_matches = matches(:,inlier_indices(1:8));
@@ -34,7 +32,7 @@ for img_n = {img_names{2+offset:end}}
     pause(0.001)
 end
 
-save('match_list.mat', 'match_list')
+save('descriptor_list.mat', 'descriptor_list')
 
-point_view_matrix = construct_pointview_matrix(match_list);
+% point_view_matrix = construct_pointview_matrix(descriptor_list);
 % save('point_view_matrix.mat', 'point_view_matrix')
